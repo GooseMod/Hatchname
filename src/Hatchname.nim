1d4aa069baa1a4f840d61efb17d23d1203cde3a7
Oj <oj@oojmed.com>
2021-08-16 21:36:56 +0100

    [Feat] Discord restarting

--- src/Hatchname.nim
+++ src/Hatchname.nim
@@ -5,6 +5,7 @@ import sequtils
 import tables
 import sugar
 import strformat
+import osproc
 
 const updatesHost = "https://updates.goosemod.com"
 
@@ -28,11 +29,36 @@ proc getChannelPath(channel: string): string =
     # Path to passed Discord channel's modules directory
     var channelPath = os.joinPath(baseDirectory, "discord")
 
-    # Discord stable doesn't have a suffix so it's ignored
+    # Discord stable doesn't have a suffix so don't append channel
     if channel != "stable":
         channelPath = channelPath & channel
     
     return channelPath
+
+
+proc quitDiscord(channel: string) =
+    var binaryName = "Discord"
+
+    if channel != "stable":
+        binaryName = binaryName & ($channel[0]).toUpper() & channel[1..channel.len]
+
+    when system.hostOS == "windows":
+        discard execShellCmd(fmt"taskkill /f /im {binaryName}.exe")
+    else:
+        discard execShellCmd(fmt"killall {binaryName}")
+
+proc startDiscord(channel: string) =
+    var binaryName = "Discord"
+    var linuxBootstrap = "discord"
+
+    if channel != "stable":
+        binaryName = binaryName & ($channel[0]).toUpper() & channel[1..channel.len]
+        linuxBootstrap = linuxBootstrap & fmt"-{channel}"
+
+    when system.hostOS == "windows":
+        discard startProcess(fmt"'%localappdata%\{binaryName}\Update.exe' --processStart {binaryName}.exe", options = {poStdErrToStdOut, poUsePath, poEvalCommand})
+    else:
+        discard startProcess(linuxBootstrap, options = {poStdErrToStdOut, poUsePath, poEvalCommand})
 
 
 let gmRemoved = fileName.split("GM")[1]
@@ -40,6 +66,10 @@ let noUnderscore = gmRemoved.split("_")
 let selectedChannel = noUnderscore[0].toLower()
 
 var updatesEndpoint: string
+
+echo("Exiting Discord...")
+quitDiscord(selectedChannel)
+sleep(3000) # Wait for Discord to finish closing
 
 # todo: Sanity check filename, if not error out
 
@@ -49,8 +79,11 @@ if noUnderscore.len == 1:
 else: # Generate endpoint from modules encoded in filename
     updatesEndpoint = (block: collect(newSeq): (for i in deduplicate(mapIt(noUnderscore[1], $it)): moduleIds[i])).join("+")
 
+echo("Injecting GooseMod...")
+
 # Get settings path via 
 let settingsPath = os.joinPath(getChannelPath(selectedChannel), "settings.json")
+
 if fileExists(settingsPath):
     let settings = parseFile(settingsPath)
 
@@ -58,5 +91,8 @@ if fileExists(settingsPath):
     settings.add("NEW_UPDATE_ENDPOINT", %fmt"{updatesHost}/{updatesEndpoint}/")
 
     writeFile(settingsPath, settings.pretty())
-else:
+# else:
     # todo: Error out (no settings.json)
+
+echo("Starting Discord...")
+startDiscord(selectedChannel)
